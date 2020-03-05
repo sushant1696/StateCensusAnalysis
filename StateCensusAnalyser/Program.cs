b3604e1d88dc51d1e9c51b4198e50197f9ac30d7
sushant1696 <shaan160196@gmail.com>
2020-03-05 16:18:09 +0530

    [Sushant] welcome message

---
+++ StateCensusAnalyser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StateCensusAnalyser
+{
+    class Program
+    {
+       
+          
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to State Census Analysis");
+        }
+
+    }
+    
+}
