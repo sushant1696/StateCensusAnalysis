c2fe6cf283782e8eb02678eb939c9323f8272509
sushant1696 <shaan160196@gmail.com>
2020-03-18 19:26:57 +0530

    [sushant] Refactore .merge the statecode and censusdata code

---
+++ StateCensusAnalyser/MergeCSVFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StateCensusAnalyser
+{
+    public class MergeCSVFile
+    {
+        List<string> list = new List<string>();
+        string StateCode_path = @"C:\Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\Files\StateCode.csv";
+        string StateCensus_path = @"C:\Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\Files\StateCensusData.csv";
+        string header  = "SrNo,State Name,TIN,StateCode,Population,AreaInSqKm,DensityPerSqKm";
+        int count;
+        public void merge()
+        {
+            list.Add(header);
+            
+            string[] StateCode = File.ReadAllLines(StateCode_path);
+            string[] CensusCode = File.ReadAllLines(StateCensus_path);
+            for(int i = 1;i< StateCode.Length;i++)
+            {
+                count = 0;
+                string[] StateCodeLine = StateCode[i].Split(',');
+                for(int j = 1; j < CensusCode.Length; j++)
+                {
+                    string[] CensusCodeLine = CensusCode[j].Split(',');
+                    mergefile(StateCode[i], CensusCode[j], StateCodeLine, CensusCodeLine);
+                }
+                if (count == 0)
+                {
+                    // It adds new State that is not common
+                    // in both csv file;
+                    string sa = String.Concat(StateCode[i]+",0,0,0,0");
+                    list.Add(sa);
+
+                }
+            }
+            File.WriteAllLines(@"C:\Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\Files\MergeOfTwoFile.csv", list);
+            Console.WriteLine();
+        }
+        public void mergefile(string _code,string _census,string[] StateCodeLine, string[] CensusCodeLine)
+        {
+            if(StateCodeLine[1]== CensusCodeLine[0])
+            {
+                int a = _census.IndexOf(",");
+                string sf = _census.Substring(a);
+                string s = String.Concat(_code, sf);
+                list.Add(s);
+                count = 1;
+            }
+            
+        }
+    }
+}
--- StateCensusAnalyser/Program.cs
+++ StateCensusAnalyser/Program.cs
@@ -24,46 +24,48 @@ namespace StateCensusAnalyser
         public string pathCSVjson = @"C:\Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\CSVjsonFile.json";
         static void Main(string[] args)
         {
-/*            Console.WriteLine("This is CSVStates class methods delegate process.......");
-            /// Create a object of delegate
-            DCsvState delegateobj = new DCsvState(new CSVStates().ReadCsvFile);
-            delegateobj(path2, ',', "SrNo,StateName,TIN,StateCode");
+            /*            Console.WriteLine("This is CSVStates class methods delegate process.......");
+                        /// Create a object of delegate
+                        DCsvState delegateobj = new DCsvState(new CSVStates().ReadCsvFile);
+                        delegateobj(path2, ',', "SrNo,StateName,TIN,StateCode");
 
-            Console.WriteLine("This is CSVStatesCensus class methods delegate process.......");
-            /// Create a object of delegate  of CSVStateCensus ReadCsvFile method
-            DelReadCsvFile delegateobjj = new DelReadCsvFile(new CSVStateCensus().ReadCsvFile);
-            delegateobjj(path, ',', "State,Population,AreaInSqKm,DensityPerSqKm");
+                        Console.WriteLine("This is CSVStatesCensus class methods delegate process.......");
+                        /// Create a object of delegate  of CSVStateCensus ReadCsvFile method
+                        DelReadCsvFile delegateobjj = new DelReadCsvFile(new CSVStateCensus().ReadCsvFile);
+                        delegateobjj(path, ',', "State,Population,AreaInSqKm,DensityPerSqKm");
 
-            Console.WriteLine("This is StateCensusAnalysis class methods delegate process.......");
-            /// Create a object of delegateOf_CSVStateCodeMethod
-            delegateOf_CSVStateCodeMethod delobj = new delegateOf_CSVStateCodeMethod(new StateCensusAnalysis().CSVStateCodeMethod);
-            delobj(path);
-            Console.WriteLine("count the no of line in the csv file");
-            Console.WriteLine(new CSVStateCensus().ReadCsvFile(path, ',', "State,Population,AreaInSqKm,DensityPerSqKm"));
-            Console.WriteLine(new CSVStates().ReadCsvFile(path2, ',', "SrNo,StateName,TIN,StateCode"));
+                        Console.WriteLine("This is StateCensusAnalysis class methods delegate process.......");
+                        /// Create a object of delegateOf_CSVStateCodeMethod
+                        delegateOf_CSVStateCodeMethod delobj = new delegateOf_CSVStateCodeMethod(new StateCensusAnalysis().CSVStateCodeMethod);
+                        delobj(path);
+                        Console.WriteLine("count the no of line in the csv file");
+                        Console.WriteLine(new CSVStateCensus().ReadCsvFile(path, ',', "State,Population,AreaInSqKm,DensityPerSqKm"));
+                        Console.WriteLine(new CSVStates().ReadCsvFile(path2, ',', "SrNo,StateName,TIN,StateCode"));
 
-            Console.WriteLine("This code for sorting the State census Analyser data in json file............");
-            JArray ja = StateCensusAnalysis.CensusAnalyserSort(pathjson);
-            var s = JsonConvert.SerializeObject(ja, Formatting.Indented);
-            File.WriteAllText(pathjson, s);
+                        Console.WriteLine("This code for sorting the State census Analyser data in json file............");
+                        JArray ja = StateCensusAnalysis.CensusAnalyserSort(pathjson);
+                        var s = JsonConvert.SerializeObject(ja, Formatting.Indented);
+                        File.WriteAllText(pathjson, s);
 
-            Console.WriteLine("This code for Convert the csv to json of CSVStateCode......  ");
-            CSVStates cs = new CSVStates();
-            cs.CSVJsonReadWrite();
+                        Console.WriteLine("This code for Convert the csv to json of CSVStateCode......  ");
+                        CSVStates cs = new CSVStates();
+                        cs.CSVJsonReadWrite();
 
-            //Console.WriteLine("csv state code sort...................");
-            //CSVStates.CSVStateCodeSort(@"C: \Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\CSVjson.json");
+                        //Console.WriteLine("csv state code sort...................");
+                        //CSVStates.CSVStateCodeSort(@"C: \Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\CSVjson.json");
 
-            Console.WriteLine("This code for sorting the csvstate in json file............");
-            string csvjsonpath = @"C:\Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\CSVjson.json";
-            JArray jarr = CSVStates.CSVStateCodeSort(csvjsonpath);
-            var sort = JsonConvert.SerializeObject(jarr, Formatting.Indented);
-            File.WriteAllText(csvjsonpath, sort);*/
+                        Console.WriteLine("This code for sorting the csvstate in json file............");
+                        string csvjsonpath = @"C:\Users\Bridgelabz\Documents\StateCensusAnalyserProject\StateCensusAnalyser\CSVjson.json";
+                        JArray jarr = CSVStates.CSVStateCodeSort(csvjsonpath);
+                        var sort = JsonConvert.SerializeObject(jarr, Formatting.Indented);
+                        File.WriteAllText(csvjsonpath, sort);*/
             /// load USCensusData
             //Console.WriteLine("Display the USCensus data...");
             //USCensusData.DisplayUSCensusData();
-            int a=USCensusData.LoadUSCensusData();
-            Console.WriteLine("length{0}",a);
+            //int a=USCensusData.LoadUSCensusData();
+            //Console.WriteLine("length:{0}",a);
+            MergeCSVFile merg = new MergeCSVFile();
+            merg.merge();
         }
 
     }
